ac288e4c8ef7abac8d1c546d2e33add5fcc71a10
Christian de Botton <debotton@brooklynunited.com>
2015-02-11 16:55:56 -0500

    Working on isomorphic jsonp.

--- app/index.js
+++ app/index.js
@@ -3,7 +3,13 @@
 import React from "react";
 import ReactRouter from "react-router";
 import router from "./router";
+import Promise from "bluebird";
 
 router.run((Handler, state) => {
+  let promises = state.routes.filter(route => route.handler.fetchData)
+    .map(route => route.handler.fetchData(state.params, state.query));
+
+  Promise.all(promises).then(data => console.log(data));
+
   React.render(<Handler {...state} />, document);
 });
--- app/utils/TumblrAPI.js
+++ app/utils/TumblrAPI.js
@@ -1,22 +1,19 @@
-import {getAPI} from "./APIUtils";
 import path from "path";
 import AppServerActionCreators from "../actions/AppServerActionCreators";
+require('es6-promise').polyfill();
+import fetch from "isomorphic-fetch";
 
-const ENDPOINT_ROOT = 'http://api.tumblr.com/v2/blog';
+const ENDPOINT_ROOT = '//api.tumblr.com/v2/blog';
 const TUMBLR = 'cbotzzz.tumblr.com';
 const API_KEY = 'fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4';
 
 export default {
   posts() {
     let url = makeUrl('posts');
-
-    return getAPI(url)
-      .then(AppServerActionCreators.requestSuccess)
-      .catch(AppServerActionCreators.requestError);
+    return fetch(url).then(data => console.log(data.body));
   }
 };
 
 var makeUrl = (resource) => {
-  return path.join(ENDPOINT_ROOT, TUMBLR, resource) +
-    `?api_key=${API_KEY}`;
+  return ENDPOINT_ROOT + path.join(TUMBLR, resource);
 };
