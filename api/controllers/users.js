be7bde14876cf20de6159644dfe5414520627245
Christian de Botton <cdebotton@gmail.com>
2015-02-12 21:44:38 -0500

    Scaffolding server.

---
+++ api/controllers/users.js
@@ -0,0 +1,23 @@
+export default (api) => {
+  api.get('users', () => {
+
+  });
+
+  api.get('users/:userId', (userId) => {
+
+  });
+
+  api.post('users', () => {
+
+  });
+
+  api.put('users/:userId', (userId) => {
+
+  });
+
+  api.del('users/:userId', (userId) => {
+
+  });
+
+  return api;
+};
